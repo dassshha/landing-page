141053baab1270f9852fba8d2a47a309c59ddbe2
dassshha <45672654+dassshha@users.noreply.github.com>
2022-10-19 19:07:10 +0300

    Delete source directory

--- source/js/data.js
+++
@@ -1,7 +0,0 @@
-export const fillLocalStorage = (lamps) => {
-    localStorage.clear();
-    lamps.forEach(lamp => {
-        localStorage.setItem(lamp.id, JSON.stringify(lamp));
-    });
-    return lamps;
-}
