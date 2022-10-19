486eb0c4e9306098e76ac315e09138ff8b035827
dassshha <dzhguleva@mail.ru>
2022-10-19 17:11:50 +0300

    add build

--- source/js/mode-button.js
+++ source/js/mode-button.js
@@ -1,5 +1,6 @@
 export const onButtonLightModeClicked = () => {
-    $('.big-picture__back').attr('src', './img/big-pic-day.jpg').attr('alt', 'Light mode');
+    $('#big-picture__back_source').attr('srcset', './img/big-pic-day@1x.webp 1x, ./img/big-pic-day@2x.webp 2x');
+    $('.big-picture__back').attr('src', './img/big-pic-day@1x.jpg').attr('srcset', './img/big-pic-day@1x.jpg 1x, ./img/big-pic-day@2x.jpg 2x').attr('alt', 'Light mode');
     $('.big-picture__lamp').css('display', 'block');
 }
 
@@ -7,7 +8,8 @@ const onButtonDarkModeClicked = async () => {
     const lampId = $('.widget-card_active').attr('id')
     const lamp = await JSON.parse(localStorage.getItem(lampId));
     if (lamp.isDarkMode) {
-        $('.big-picture__back').attr('src', './img/big-pic-night.jpg').attr('alt', 'Dark mode');
+        $('#big-picture__back_source').attr('srcset', './img/big-pic-night@1x.webp 1x, ./img/big-pic-night@2x.webp 2x');
+        $('.big-picture__back').attr('src', './img/big-pic-night@1x.jpg').attr('srcset', './img/big-pic-night@1x.jpg 1x, ./img/big-pic-night@2x.jpg 2x').attr('alt', 'Dark mode');
         $('.big-picture__lamp').css('display', 'none');
     }
 
